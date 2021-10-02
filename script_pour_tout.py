7415c68ec047163a5eb318eb11be19151d16cd67
Marchand Christopher <jean.random620@gmail.com>
2021-10-02 06:13:14 +0200

    récupération et stockage des liens des livres ou les categories n'ont pas de pagination

--- script_pour_tout.py
+++ script_pour_tout.py
@@ -4,67 +4,60 @@ import csv
 
 lien_du_site = "http://books.toscrape.com/"
 
-information = {"universal_product_code" : [],
-               "price_including_tax" : [],
-               "price_excluding_tax" : [],
-               "number_avaible" : [], 
-               "title" : [],
-               "product_description" : [],
-               "category" : [],
-               "review_rating" : [],
-               "image_url" : [], 
-               "product_page_url" : []} 
+base_url = "http://books.toscrape.com/index.html"
 
-urls_categories = []
+index_des_categories = []
 lien_de_tout = []
 nombre_de_page = []
 
+liens_a_parser = {}
+
+information = {
+    "universal_product_code" : [],
+    "price_including_tax" : [],
+    "price_excluding_tax" : [],
+    "number_avaible" : [], 
+    "title" : [],
+    "product_description" : [],
+    "category" : [],
+    "review_rating" : [],
+    "image_url" : [], 
+    "product_page_url" : []
+               }  
+
 def recuperation_des_categories () :
-    url = "http://books.toscrape.com/index.html"
-    page = requests.get(url)
+    page = requests.get(base_url)
     index = BeautifulSoup(page.content, 'html.parser')
     for x in index.find("div", class_="side_categories").find_all("li"):
         a = x.find("a")
-        try:
-            if'href' in a.attrs:
-                url = a.get('href')
-                lien_cathegories = lien_du_site+url
-                urls_categories.append(lien_cathegories)
-        except:
-            print("marche pas la la la" )
-    del urls_categories[0]
-    return urls_categories
+        if'href' in a.attrs:
+            url = a.get('href')
+            lien_cathegories = lien_du_site+url
+            index_des_categories.append(lien_cathegories)
+    del index_des_categories[0]
+    return index_des_categories
 
 recuperation_des_categories()
 
-def recuperation_des_livres(urls_categories):
-    liste_page = []
-    urls = []
-    for liens in urls_categories:
-        liste_page.append(liens)
-        page = requests.get(liens)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        test = soup .find("li", class_="next")
-        print(liens)
-        if test == None :
-            for liste_livre in liste_page :
-                page_livre = requests.get(liste_livre)
-                soup_de_livre = BeautifulSoup(page_livre.content, 'html.parser')
-                test = soup_de_livre.select("h3 a")
-                for x in soup_de_livre.find_all("h3"):
-                    a = x.find("a")
-                    try:
-                        if'href' in a.attrs:
-                            url = a.get('href').replace("../../../","http://books.toscrape.com/catalogue/")
-                            urls.append(url)
-                            print([data_extraction(item) for item in urls])
-                    except:
-                        print("marche pas la la la" )
-        else:
-            print(test)
+def parse_categories():
+    for categories in index_des_categories :
+        page_categories = requests.get(categories)
+        if page_categories.status_code == requests.codes.ok:
+            page_a_parser = BeautifulSoup(page_categories.content, "html.parser")
+        find_next = page_a_parser.find("li", class_="next")
+        find_previous = page_a_parser.find("li", class_="previous")
+        if find_next == None :
+            urls = []
+            nom_de_la_categorie = page_a_parser.find("li", class_="active").text
+            for lien in page_a_parser.find_all("h3"):
+                a_tag = lien.find("a")
+                url = a_tag.get('href').replace("../../../","http://books.toscrape.com/catalogue/")
+                urls.append(url)
+            liens_a_parser.update({nom_de_la_categorie:urls})
+        if find_next != None or find_previous != None:
+            pass
 
-
-def data_extraction(item):   
+def data_extraction(item):
     items = requests.get(item)
     soup = BeautifulSoup(items.content, 'html.parser')
     donnee_livre = []
@@ -81,14 +74,6 @@ def data_extraction(item):
     information['review_rating'].append(list(note_du_livre.values()))# on extrai uniquement la note qui est une valeur du dictionnaire
     information['image_url'].append(soup.find("img").get("src").replace("../../", "http://books.toscrape.com/"))#on ajoute la lien de lien de l'image
     information['product_page_url'].append(item)
-    print(information)
-    
+    return information
 
-
-recuperation_des_livres(urls_categories)
-
-
-
-
-
-
+parse_categories()
